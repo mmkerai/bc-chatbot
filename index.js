149e9c9836d57fc5e9f701c504cf188b1566dc3e
thecodecentre <admin@tropicalfruitandveg.com>
2017-07-31 14:20:40 +0100

    operator select

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 var socket = io.connect();
 const MONITORROOM = "monitor_room";		// socket room name for monitoring this service
-
+var Operators = new Object();
+/*Operators["12345"] = "Op1";
+Operators["23456"] = "Op2";
+*/
 function getDepartments() {
 	socket.emit('departmentRequest',"");
 }
 
-function getOperators() {
-	socket.emit('operatorRequest',"");
+function setOperators() {
+
 }
 
-function startBot() {
+function startMonitor() {
 	clearall();
 	$('#dlog').show();
 	socket.emit('join room',MONITORROOM);
 }
 
-function stopBot() {
+function stopMonitor() {
 	socket.emit('leave room',MONITORROOM);
 }
 
 $(document).ready(function() {
 
 	console.log("Console Ready");
+	socket.emit('operatorRequest',"");
 	clearall();
-	socket.on('errorResponse', function(data){
+	socket.on('errorResponse', function(data) {
 		clearall();
 		$("#error").text(data);
 	});
@@ -44,21 +48,16 @@ $(document).ready(function() {
 		$("#message1").html(str);
 	});
 	// this returns an object of operator objects
-	socket.on('operatorResponse',function(data){
-		clearall();
-		var str = "No of Operators: "+Object.keys(data).length;
-		str += "<table><tr><td>OperatorID</td><td>Name</td><td>Chat Messages</td></tr>";
-		for(var i in data)
+	socket.on('operatorResponse',function(dlist) {
+		for(var i in dlist)
 		{
-			str += "<tr><td>"+data[i].operatorID+"</td>";
-			str += "<td>"+data[i].operatorName+"</td>";
-			str += "<td>"+data[i].chatMessages+"</td>";
+			Operators[dlist[i].operatorID] = dlist[i].operatorName;
 		}
-		str += 	"</table>";
-		$("#message1").html(str);
+		var str = "No of Operators: "+Object.keys(Operators).length;
+		populateOperatorsSelect();
 	});
 	// this returns a message object
-	socket.on('chatMessage',function(data){
+	socket.on('chatMessage',function(data) {
 		var str = "\r\n";
 		console.log("Message: "+data.text);
 		str += "Chat id:"+data.chatID+"\r\n";
@@ -68,12 +67,23 @@ $(document).ready(function() {
 		$('#dlog').append(str);
 		document.getElementById("dlog").scrollTop = document.getElementById("dlog").scrollHeight
 	});
-	socket.on('consoleLogs',function(data){
+	socket.on('consoleLogs',function(data) {
 		$('#dlog').append(data+"\r\n");
 		document.getElementById("dlog").scrollTop = document.getElementById("dlog").scrollHeight
 	});
 });
 
+function getSelected(opsel) {
+	var opid = opsel.val();
+	console.log("Operator selected: "+opid);
+}
+/*
+$('#opform').submit(function(event) {
+	event.preventDefault();
+	initialiseValues();
+	opid = $("#OpSelect option:selected").val();
+});
+*/
 function clearall() {
 	$('#chatform').hide();
 	$('#dlog').hide();
@@ -82,6 +92,30 @@ function clearall() {
 	$('#message2').html("");
 }
 
+function populateOperatorsSelect() {
+	var opselect = document.getElementById("OpSelect");
+	var option;
+	for(var i in Operators)
+	{
+		option = document.createElement("option");
+		option.text = Operators[i];
+		option.value = i;
+		opselect.appendChild(option);
+	}
+}
+
+function showOperators() {
+	clearall();
+	var str = "No of Operators: "+Object.keys(Operators).length;
+	str += "<table><tr><td>OperatorID</td><td>Name</td></tr>";
+	for(var i in Operators)
+	{
+		str += "<tr><td>"+i+"</td>";
+		str += "<td>"+Operators[i]+"</td></tr>";
+	}
+	str += 	"</table>";
+	$("#message1").html(str);
+}
 /*
  *	This function makes data (typically csv format) available for download
  *  using the DOM id "download" which should be labelled "download file"
